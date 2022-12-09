ec596585ab6dce7b8854020f02c128aa05e4235e
Yanis Madi <95629416+YanisMadi@users.noreply.github.com>
2022-12-09 15:56:16 +0100

    Update stat_analysis.r

--- bin/stat_analysis.r
+++ bin/stat_analysis.r
@@ -1,110 +1,207 @@
-#!/usr/bin/Rscript --slave
-###############################################################################
-########################      STATISTICAL ANALYSIS      #######################
-###############################################################################
-
-
-#### Description
-# This script performs a statistical analysis of the count matrix obtained by the workflow. It uses the DESeq2 and FactoMineR packages.
-# It takes the count file as input and returns various files as output. The files which are returned are a PCA graph, the table of the 
-# results of the differential expression analysis, a plot of the counts of the gene most differentially expressed between the two groups,
-# the heatmap of the normalized counts of the most variable genes and the name of these. genes in a text file.
-
-
-#### Library Loading
-install.packages("FactoMineR")  # Installation of the package to perform the PCA
-library(DESeq2)                 # Loading package for differential analysis
-library(FactoMineR)             # Loading packahe for the PCA
-
+## HACKATON ANALYSIS
 
 #### Data Loading
 # Counts Matrix
 args = commandArgs(T)
 counts_file = args[1]
 dir = args[2]
-path = paste(dir, "/count/", sep="")
-count_file = read.table(paste(path, counts_file, sep=""), header=T)    # Loading count matrix file
-counts = count_file[,7:14]
-rownames(counts) = count_file[,1]
+path1 = paste(dir, "/count/", sep="")
+path2 = paste(dir, sep="")
 
-parserexpcolnames <- function(nomamodifier){     # remove ".bam" in colnames function
-  nomamodifier <- substring(nomamodifier, 1, 9)
-  return(nomamodifier)
-}
-
-colnames(counts) = sapply(colnames(counts), parserexpcolnames)   # remove ".bam" in colnames
-
-# Annotation Phenotype
-list_phenotype = c()   # list containing phenotype of each SRR
-for (i in 1:dim(counts)[2]){
-  if((colnames(counts)[i] == 'SRR628582') | (colnames(counts)[i] == 'SRR628583') | (colnames(counts)[i] == 'SRR628589')){
-    list_phenotype = c(list_phenotype, "Mutated")
-  }
-  else{
-    list_phenotype = c(list_phenotype, "Wild")
-  }
-}
-phenotype = as.data.frame(matrix(list_phenotype, ncol = 1))
-rownames(phenotype) = colnames(counts)
-colnames(phenotype) = "Type"
+meta<-read.table(paste(path2,"metaData.txt"), header = TRUE, sep = ",",row.names = 1)
+Counts = read.table(paste(path2, counts_file, sep=""), header=T)    # Loading count matrix file
 
 
+##### Set Seed #####
+set.seed(123)
 
-#### Analysis
+#### PACKAGES
+# install.packages("pheatmap")
+# install.packages("factoextra")
+# if (!requireNamespace('BiocManager', quietly = TRUE)){
+#   install.packages('BiocManager')
+# }
+# BiocManager::install('EnhancedVolcano')
+# BiocManager::install('apeglm')
+# install.packages("magrittr") # package installations are only needed the first time you use it
+# install.packages("dplyr")    # alternative installation of the %>%
+install.packages("plotly")
+library("DESeq2")
+library("pheatmap")
+library("FactoMineR")
+library("factoextra")
+library("tidyverse")
+library("FactoMineR")
+library("apeglm")
+library ("RColorBrewer") 
+library("ggrepel")
+library("EnhancedVolcano")
+library("magrittr") # needs to be run every time you start R and want to use %>%
+library("dplyr")    # alternatively, this also loads %>%
+library("ggplot2")
+library("plotly")
 
-## PCA
 
-res_pca = PCA(t(counts), graph= FALSE)              # Calculation of PCA
-pdf("PCA_GraphOfIndividuals.pdf")
-plot(res_pca, axes = c(1, 2), choix = c("ind"))     # Calculation is made in 5 dimensions, and the display in 2 
+# Data formatting and Data cleaning --------------------------------------------
+
+## Dataframe
+# Removing the first 5 columns (unecessary for analysis) 
+new_Counts= Counts[,-c(1:5)]
+# Removing ".bam" extension in column names
+colnames(new_Counts)[c(1:8)] = gsub('.{0,4}$', '', colnames(new_Counts)[c(1:8)])
+colnames(new_Counts)
+# Sorting in order : 
+new_Counts<-new_Counts[,rownames(meta)]
+colnames(new_Counts)
+all(rownames(meta) == colnames(new_Counts))
+
+## Labels
+labels = c("SRR628582", "SRR628583", "SRR628584", "SRR628585", "SRR628586", "SRR628587", "SRR628588", "SRR628589")
+mutations = c("WT", "M", "WT", "WT", "WT", "WT", "M", "M")
+labels2 = data.frame(labels, mutations)
+
+## Ordering the tissue types so that it is sensible and make sure the control sample is first: WT -> M:mutÃ©
+labels2$mutations<-factor(labels2$mutations,levels=c("WT","M"))
+## Removal of all unexpressed genes
+dds <- DESeqDataSetFromMatrix(countData=new_Counts, colData=labels2, design= ~ mutations)
+dds <- DESeq(dds) # Levels: M WT
+dds$sizeFactor
+res <- results(dds)
+summary(res)
+new_Counts = new_Counts[rowSums(new_Counts)>0,]   
+new_Counts=as.matrix(new_Counts) 
+new_Counts<- data.frame(new_Counts)
+#head(new_Counts)
+
+# I- RNA-seq count distribution ------------------------------------------------
+
+hist=ggplot(new_Counts) +geom_histogram(aes(x =SRR628582), stat = "bin", bins = 200) + xlim(-5, 500)  +xlab("Raw expression counts") + ylab("Number of genes")
+png("rawcount_vs_nbgene.png",width=600)
+plot(hist)
 dev.off()
 
+## Modeling count data for Mutated samples -------------------------------------
 
-## Differencial Analysis
-
-counts = counts[rowSums(counts)>0,]   # Removal of all unexpressed genes
-counts=as.matrix(counts)              # Converting the object's class
-
-
-dds = DESeqDataSetFromMatrix(countData = counts, colData = phenotype, design = ~ Type)  # Object construction
-dds = DESeq(dds)     # performs differential expression analysis
-res = results(dds)   # differential expression analysis
-res_mat = as.matrix(res)
-write.table(res_mat, "DESeq_results.txt", sep="\t")  # output file (table format txt) resulting of the differential expression analysis
-
-pdf("plot_counts.pdf")
-plotCounts(dds, gene=which.min(res$padj), intgroup="Type")  # output file (pdf) showing the most variable gene bewteen the 2 groups (wild and mutated)
+mean_counts <- apply(new_Counts[c(2,7,8)], 1, mean)
+variance_counts <- apply(new_Counts[c(2,7,8)], 1, var)
+df <- data.frame(mean_counts, variance_counts)
+Disp=ggplot(df) + geom_point(aes(x=mean_counts, y=variance_counts)) + geom_line(aes(x=mean_counts, y=mean_counts, color="red")) + scale_y_log10() + scale_x_log10()
+# exporting figure
+png("MeanVariance_Mutatedsamples.png")
+plot(Disp)
 dev.off()
 
-DEgenes = which(res$padj<0.05)      # row numbers whose genes are significantly differentially expressed
-significative_DEgenes = res[DEgenes,]   # matrix containing names, log2FC and adjusted p-value of significantly differentially expressed genes
-significative_DEgenes = significative_DEgenes[, c(2,6)]
-write.table(significative_DEgenes, "Significative_DEgenes.txt", sep="\t")   # output file (matrix format txt) of previous matrix 
+## Modeling count data for WT samples ------------------------------------------
 
-summary.df = as.data.frame(matrix("", ncol= 3, nrow = 3))  # DataFrame whose summarise previous matrix
-rownames(summary.df) = c("significantly differentially expressed ", "overexpressed genes in mutants", "overexpressed genes in wild types")
-colnames(summary.df) = c("Amount", "Ratio (significantly differentially expressed)", "Ratio (all expressed genes)")
-
-summary.df[1,1] = dim(significative_DEgenes)[1]
-summary.df[2,1] = length(which(significative_DEgenes$log2FoldChange < -1))
-summary.df[3,1] = length(which(significative_DEgenes$log2FoldChange > 1))
-
-summary.df[1,2] = round(as.numeric(summary.df[1,1]) / as.numeric(summary.df[1,1]), digits = 2)
-summary.df[2,2] = round(as.numeric(summary.df[2,1]) / as.numeric(summary.df[1,1]), digits = 2)
-summary.df[3,2] = round(as.numeric(summary.df[3,1]) / as.numeric(summary.df[1,1]), digits = 2)
-
-summary.df[1,3] = round(as.numeric(summary.df[1,1]) / dim(counts)[1], digits = 2)
-summary.df[2,3] = round(as.numeric(summary.df[2,1]) / dim(counts)[1], digits = 2)
-summary.df[3,3] = round(as.numeric(summary.df[3,1]) / dim(counts)[1], digits = 2)
-
-write.table(summary.df, "Significative_DEgenes_Summary.txt", sep="\t")   # output file (matrix format txt) of previous DataFrame
-
-select = order(res$padj, decreasing = FALSE)[1:30]  # Sorting and selection of the most variable genes
-
-pdf("heatmap_MostVariableGenes.pdf")
-heatmap(counts(dds, normalized=T)[select,], cexRow = 0.5, cexCol = 0.8, Rowv = NA)   # output file (pdf) showing normalized counts of the most variable genes for each individual
+mean_counts <- apply(new_Counts[c(1,3,4,5,6)], 1, mean)
+variance_counts <- apply(new_Counts[c(1,3,4,5,6)], 1, var)
+df <- data.frame(mean_counts, variance_counts)
+Disp=ggplot(df) + geom_point(aes(x=mean_counts, y=variance_counts)) + geom_line(aes(x=mean_counts, y=mean_counts, color="red")) + scale_y_log10() + scale_x_log10()
+# exporting figure
+png("MeanVariance_WTsamples.png")
+plot(Disp)
 dev.off()
 
-MostVariableGenes = rownames(counts[select,])  # Retrieving names names of the most variable genes
+# II - ANALYSIS ----------------------------------------------------------------
 
-write.table(MostVariableGenes, "MostVariableGenes.txt", sep=";")   # output file (list format txt) containing names of the most variable genes
+## ACP -------------------------------------------------------------------------
+
+tran_data<-t(new_Counts)
+res_pca<-PCA(tran_data,graph = FALSE)
+#res_pca$eig
+plot_pca=fviz_pca_ind (res_pca,
+                       repel = TRUE,
+                       col.ind = mutations, # coloring by tissue type
+                       palette = c("#009999", "#0000FF"),
+                       legend.title = "Sample Type",
+                       addEllipses = TRUE,
+                       ellipse.level=0.95,
+                       mean.point = FALSE)
+png("PCA.png",width=600)
+plot(plot_pca)
+dev.off()
+
+## HEATMAP ---------------------------------------------------------------------
+
+## Data
+#Transform normalized counts using the rlog transformation
+rld<-rlog(dds,blind=TRUE)
+# Extract the rlog matrix from the object
+rld_mat <- assay(rld)
+# Compute pairwise correlation values
+rld_cor <- cor(rld_mat)
+
+## Plot heatmap
+heat.colors <- brewer.pal(6, "Purples")
+# exporting
+# png("heatmap.png",width=600)
+# heatmap=pheatmap(rld_cor, color = heat.colors, border_color=NA, fontsize = 10, 
+#                  fontsize_row = 10, height=20)
+# dev.off()
+
+##
+sizeFactors(dds)
+res <- results(dds)
+res<- results(dds,contrast = c("mutations","M","WT"))
+class(res)
+summary(res)
+mcols(res, use.names=T)
+#res %>% data.frame() %>% View()
+
+# Remoing 'NA' values
+res_NO_NA<-res[complete.cases(res[,6]),]
+#res_NO_NA %>% data.frame() %>% View()
+
+
+## Parameters
+p_adj<-0.05
+LFC<-0.5
+res_sig<-res_NO_NA%>%data.frame() %>%rownames_to_column(var="gene") %>% as_tibble()
+sig_DE_gene <- res_sig %>%filter(padj < p_adj & abs(log2FoldChange) >LFC)
+write.csv(sig_DE_gene, file="gÃ¨nes_df_sig.csv", row.names = FALSE )
+summary(sig_DE_gene)
+
+## Pvalue histogram
+#exporting
+png("histogram_pvalue.png")
+hist(res_sig$pvalue)
+dev.off()
+
+sig_DE_gene 
+
+top10_sigDE_genes <- sig_DE_gene %>% data.frame() %>%arrange(padj) %>% pull(gene) %>% head(n=10) 
+info_top_10_gene<- res_sig %>%filter(padj < p_adj & abs(log2FoldChange) >LFC)  %>%arrange(padj) %>% head(n=10)
+DF_to_10_gene<-as.data.frame(info_top_10_gene)
+write.csv(DF_to_10_gene, file="DF_TOP_10.csv", row.names = FALSE )
+
+lab_italics <- paste0("italic('", rownames(res_NO_NA), "')")
+selectLab_italics = paste0(
+  "italic('",
+  c(top10_sigDE_genes),
+  "')")
+png("volcano.png",width= 700, height = 500)
+EnhancedVolcano(res_NO_NA,
+                lab = lab_italics,
+                x = 'log2FoldChange',
+                y = 'pvalue',
+                title='WT vs Mutated with p_adj',
+                selectLab = selectLab_italics,
+                xlab = bquote(~Log[2]~ 'fold change'),
+                pCutoff = p_adj,
+                FCcutoff = LFC,
+                pointSize = 3.0,
+                labSize = 6.0,
+                labCol = 'black',
+                labFace = 'bold',
+                boxedLabels = TRUE,
+                parseLabels = TRUE,
+                col = c('purple', 'red3', 'black', 'pink'),
+                colAlpha = 4/5,
+                legendPosition = 'bottom',
+                legendLabSize = 14,
+                legendIconSize = 4.0,
+                drawConnectors = TRUE,
+                widthConnectors = 1.0,
+                colConnectors = 'black') + coord_flip()
+
+dev.off()
