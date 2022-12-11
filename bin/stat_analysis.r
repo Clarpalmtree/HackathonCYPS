133eba2daa54dbb68775b79621ca73a7b8b2e70b
plim <pauline.lim0299@gmail.com>
2022-12-11 14:25:13 +0100

    comments

--- bin/stat_analysis.r
+++ bin/stat_analysis.r
@@ -1,22 +1,18 @@
 #!/usr/bin/Rscript --slave
 
-# rm(list=objects())
-# graphics.off()
 set.seed(123)
-
 library("DESeq2")
 
 args = commandArgs(T)
 counts_file = args[1]
 dir = args[2]
 
-meta<-read.table(paste(dir, "/metaData.txt", sep=""), header = TRUE, sep = ",",row.names = 1)
-Counts = read.table(paste(dir, "/count/output.counts", sep=""), header=T)    # Loading count matrix file
-# ## DATA ------------------------------------------------------------------------
-# meta<-read.table("metaData.txt", header = TRUE, sep = ",",row.names = 1)
-# Counts<-read.table("output.counts", header = TRUE, sep = "",row.names = 1)
+## DATA ------------------------------------------------------------------------
 
-# Data formatting and Data cleaning --------------------------------------------
+Counts = read.table(paste(dir, "/count/output.counts", sep=""), header=T) 
+meta<-read.table(paste(dir, "/metaData.txt", sep=""), header = TRUE, sep = ",",row.names = 1)
+
+## Data formatting and Data cleaning -------------------------------------------
 
 ## Dataframe
 # Removing the first 5 columns (unecessary for analysis) 
@@ -25,7 +21,6 @@ new_Counts= Counts[,-c(1:5)]
 colnames(new_Counts)[c(1:dim(new_Counts)[2])] = gsub('.{0,4}$', '', colnames(new_Counts)[c(1:dim(new_Counts)[2])])
 # Sorting in order : 
 new_Counts<-new_Counts[,rownames(meta)]
-
 print("Is data correctly sorted as in metadata ?")
 print(all(rownames(meta) == colnames(new_Counts)))
 
@@ -44,18 +39,15 @@ for (label in labels){
 labels2 = data.frame(labels, mutations)
 labels2$mutations<-factor(labels2$mutations,levels=c("WT","M"))
 
-
+## Removal of all unexpressed genes
 new_Counts = new_Counts[rowSums(new_Counts)>0,]   
 new_Counts = as.matrix(new_Counts) 
 new_Counts <- data.frame(new_Counts)
-
-## Removal of all unexpressed genes
 dds <- DESeqDataSetFromMatrix(countData=new_Counts, colData=labels2, design= ~ mutations)
-dds$mutations <- relevel(dds$mutations, ref ="WT") #set WT as reference for log2FC
-dds <- DESeq(dds) # Levels: WT M
+dds$mutations <- relevel(dds$mutations, ref ="WT") #setting WT as reference for log2FC
+dds <- DESeq(dds) # Levels : WT M
 res <- results(dds)
-## Removing NA values
-res <- na.omit(res)
+res <- na.omit(res) #Removing NA values
 
 ## PCA -------------------------------------------------------------------------
 
@@ -73,21 +65,27 @@ rld<-rlog(dds,blind=TRUE)
 rld_mat <- assay(rld)
 # Compute pairwise correlation values
 rld_cor <- cor(rld_mat)
+#export
 png("heatmap_de.png",width=700, height=600)
 heatmap(rld_cor)
 dev.off()
 
 ## Differential analysis -------------------------------------------------------
+
 ## Calculating genes parameters
 res_mat = as.matrix(res)
+#export
 write.csv(res_mat, "DESeq_results.csv")
 ## DE genes
 DEgenes = which(res$padj<0.05) # DE genes with pvalue < 5%
 significative_DEgenes = res[DEgenes,]   # matrix containing names, log2FC and adjusted p-value of significantly differentially expressed genes
 significative_DEgenes = significative_DEgenes[, c(2,6)]
 significative_DEgenes <- significative_DEgenes[order(significative_DEgenes$padj),]
+#export
 write.csv(significative_DEgenes, "significative_DEgenes.csv") 
+## Top 10 DE genes
 top_10=significative_DEgenes[1:10, ]
+#export
 write.csv(top_10,"top_10_de_genes.csv")
 
 ## Summary table -----------------------------------------------------------------
@@ -98,8 +96,7 @@ colnames(summary.df) = c("Amount", "Ratio (significantly differentially expresse
 tot = dim(significative_DEgenes)[1]
 summary.df[1,1] = length(which(significative_DEgenes$log2FoldChange < -1)) #overexpressed in WT
 summary.df[2,1] = length(which(significative_DEgenes$log2FoldChange > 1)) #overexpressed in M
-
 summary.df[1,2] = round(as.numeric(summary.df[1,1]) / tot, digits = 2)
 summary.df[2,2] = round(as.numeric(summary.df[2,1]) / tot, digits = 2)
-summary.df
+#export
 write.csv(summary.df, "summary.csv")
